5a87143c82b8ab9ac4c0241e09ed0cd0110e810a
NazarMandryk <nazarmandryk4@gmail.com>
2023-02-27 04:24:14 +0200

    c1

---
+++ try4/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        TeX t1 = new TeX();
+        t1.showThis();
+        TeX.showThis();
+
+
+
+
+    }
+    void yess(){
+
+    }
+
+}
---
+++ try5/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+class TeX{
+    public int a = 5;
+   public static void showThis(){
+        System.out.println("yess");
+    }
+}
